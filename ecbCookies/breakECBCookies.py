4a17361e13f2172a4d23509bcbe37accb77260c3
apandey16 <apande02@calpoly.edu>
2024-02-20 21:41:38 -0800

    it works? and also ebc

--- ecbCookies/breakECBCookies.py
+++ ecbCookies/breakECBCookies.py
@@ -1,17 +1,25 @@
 import requests
-import re
 
 homeURL = 'http://localhost:8080'
 regesterURL = 'http://localhost:8080/register'
 
+# WHY WAS IT 32 BYTES?
+
 session = requests.Session()
 userAdmin = "xxxxxxxxxxxadmin"
+# print(len(userAdmin))
 userAdmin = [*userAdmin]
-userAdmin += '\x00' * (11 - 1) + chr(11)
+userAdmin += '\x00' * (10) + '\x0b'
+# print(userAdmin)
+userAdminStr = ""
+for i in userAdmin:
+    userAdminStr += i
 userAdmin = "".join(userAdmin)
+# print(len(userAdmin))
 
 # get rid of the last block (16 bytes)
 uidAtBlockEnd = {"user" : "xxxxxxxxxxxxxxx", "password" : "password"}
+
 # only take the second block and put that at the end of the previous cookie
 registerPayloadAdmin = {"user" : userAdmin, "password" : "password"}
 
@@ -21,24 +29,30 @@ session.post(homeURL, data=uidAtBlockEnd)
 
 requests.get('http://localhost:8080/home')
 cookieOne = session.cookies.get_dict().get('auth_token')
-cookieOne = cookieOne[: (len(cookieOne) -16 )]
-print(cookieOne)
+# print(cookieOne)
+cookieOne = cookieOne[: (len(cookieOne) -32 )]
+# print(cookieOne)
 
 # for cookie two
 session.post(regesterURL, data=registerPayloadAdmin)
 session.post(homeURL, data=registerPayloadAdmin)
+
 requests.get('http://localhost:8080/home')
 cookieTwo = session.cookies.get_dict().get('auth_token')
-cookieTwo = cookieTwo[16:32]
-print(cookieTwo)
+# print(cookieTwo)
+cookieTwo = cookieTwo[32:64]
+# print(cookieTwo)
 
 
 cookie = cookieOne + cookieTwo
 print(cookie)
 
-#one of the blocks where role equals is at the end of a block
+session.cookies.set('auth_token', cookie)
+result = session.get('http://localhost:8080/home')
+print(result.text)
+
+# one of the blocks where role equals is at the end of a block
 # one cookie where the username is admin
-#http request to get admin as 
+# http request to get admin as 
 # padding admin padding
 # or find a user with valid padding bytes 
-# role = admin ; user = admin
